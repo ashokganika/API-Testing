ebe8cb24a7dc12b8be1e09d8a05415585963c765
Mr.A <ashok.ag.ganika@gmail.com>
2020-06-07 21:36:03 +0545

    Updates

---
+++ precache-manifest.1762dcd8eb3f8749a01dfec423f5f371.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1373102f62d8e5f88aba860ef9606433",
+    "url": "/react-github-test/index.html"
+  },
+  {
+    "revision": "3ca710d49a05e6c86f8a",
+    "url": "/react-github-test/static/css/main.aa3314a0.chunk.css"
+  },
+  {
+    "revision": "7f37973fc8b1128eadbb",
+    "url": "/react-github-test/static/js/2.10cd3623.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-github-test/static/js/2.10cd3623.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ca710d49a05e6c86f8a",
+    "url": "/react-github-test/static/js/main.72b6dc10.chunk.js"
+  },
+  {
+    "revision": "969e62a0d52a71ae2bd8",
+    "url": "/react-github-test/static/js/runtime-main.30e7d71e.js"
+  }
+]);
